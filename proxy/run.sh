a6f2a6fba8819230206212daa904c41b38980a44
julianagmendes <149825710+julianagmendes@users.noreply.github.com>
2023-12-31 10:38:17 -0500

    changed to gunicorn

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+# Generate Nginx configuration from template
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+# Start Gunicorn with default settings
+gunicorn your_django_app.wsgi:application -b 0.0.0.0:${APP_PORT} &
+
+# Start Nginx in the foreground
 nginx -g 'daemon off;'
