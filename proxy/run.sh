9c0e5ef9ccbe227de49d8d8346f420f5c96136c1
julianagmendes <149825710+julianagmendes@users.noreply.github.com>
2023-12-31 11:02:30 -0500

    changed to gunicorn

--- proxy/run.sh
+++ proxy/run.sh
@@ -6,7 +6,7 @@ set -e
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
 # Start Gunicorn with default settings
-gunicorn your_django_app.wsgi:application -b 0.0.0.0:${APP_PORT} &
+gunicorn PipeDriveAutomation.wsgi:application -b 0.0.0.0:${APP_PORT} &
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
