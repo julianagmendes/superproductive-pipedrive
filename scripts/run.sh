12bea26dd9527f7bc3261c393e01ffe1a60c8f41
julianagmendes <149825710+julianagmendes@users.noreply.github.com>
2024-01-25 10:18:40 -0500

    added uwsgi pass

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,5 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module PipeDriveAutomation.wsgi
