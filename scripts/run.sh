3f62e9fc5a8adb03adfc878ee9feac561ef0c5e7
julianagmendes <149825710+julianagmendes@users.noreply.github.com>
2024-01-26 13:58:27 -0500

    added logging

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,16 @@
 
 set -e
 
+
+# Ensure that necessary directories exist
+mkdir -p /vol/web/logs
+
+# Touch the error.log file
 touch /vol/web/logs/error.log
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py makemigrations
+python manage.py runserver 0.0.0.0:8000
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module PipeDriveAutomation.wsgi
